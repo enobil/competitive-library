c8ebf9323f9b81941044488fea1119b2f40cfcf6
Engin Oruc Ozturk <engin.linux@gmail.com>
2016-09-18 21:46:49 +0300

    Improvements for bit manipulation functions

--- Bit Manipulation/BitManipulation.hpp
+++ Bit Manipulation/BitManipulation.hpp
@@ -1,21 +1,27 @@
 #define BITMANIPULATION_H
 #include <cassert>
 
-int count1s(int n) {
+int setBits_naive(unsigned long long n) {
 	int r = 0;
-	while (n) { r += n % 2; n /= 2; }
+	while (n) { r += n % 2; n >>= 1; }
 	return r;
+}
+
+int setBits_optimized(unsigned long long n){
+    int r = 0;
+    while (n) { n &= (n - 1); ++r; }
+    return r;
 }
 
 int lg2(int n) {
 	int r = -1;
 	assert(n > 0);
-	while (n) { r++; n /= 2; }
+	while (n) { ++r; n >>= 1; }
 	return r;
 }
 
-int pow2Ceil(int n) {
+unsigned long long pow2Ceil(int n) {
 	if (count1s(n) == 1)
 		return n;
-	return 1 << (1 + lg2(n));
+	return (unsigned long long)1 << (1 + lg2(n));
 }
